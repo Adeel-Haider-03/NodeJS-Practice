c64e990c57c095ea22809c8caa75ed312fe7635d
Adeel Haider <146270709+Adeel-Haider-03@users.noreply.github.com>
2025-07-24 23:02:55 +0500

    Update database.js

--- devTinder/src/config/database.js
+++ devTinder/src/config/database.js
@@ -1,4 +1,4 @@
-const URI="mongodb+srv://adeelboss3360:cwzFv7O7VnWjYy4c@cluster0.biodaoa.mongodb.net/devTinder"
+const URI=""
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
