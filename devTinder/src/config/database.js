c6ed183f0b21588df4c7584fa401b9308e56783f
Adeel Haider <146270709+Adeel-Haider-03@users.noreply.github.com>
2025-07-27 22:19:54 +0500

    Update database.js

--- devTinder/src/config/database.js
+++ devTinder/src/config/database.js
@@ -1,8 +1,8 @@
-const URI=""
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(URI)
+    await mongoose.connect(process.env.MONGODB_URI)
 }
 
 module.exports = connectDB;
