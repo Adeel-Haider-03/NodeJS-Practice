b7aa99f995a806a563c9742848fe9d94c4a113c7
Adeel Haider <146270709+Adeel-Haider-03@users.noreply.github.com>
2025-08-16 23:51:09 +0500

    Add files via upload

---
+++ devTinder/src/routes/auth.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router=express.Router()
+const { validateSignupData,validateLoginData } = require('./utils/helper');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+
+
+
+router.post('/signup', async (req, res) => {
+    
+    try {
+        //user validation
+        const isValid = validateSignupData(req.body);
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        if(isValid){
+
+            //hashing password
+            const hashPassword=await bcrypt.hash(password, 10);
+
+            const user=new User({
+                firstName,
+                lastName,
+                emailId,
+                password: hashPassword //store hashed password
+            })
+            await user.save()
+            res.status(200).send("user saved successfully")
+
+        }
+    
+    } catch (error) {
+        res.status(400).send("ERROR:"+ error.message);
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const isvalidlogin=validateLoginData(req.body)
+
+        const {emailId,password}=req.body
+
+        if(isvalidlogin){
+            const user=await User.findOne({emailId:emailId})  //find user
+            if(!user){
+                throw new Error ("Invalid credientials")
+            }
+            
+            const isvalidPassword=user.validatePassword(password)      //user password in DB is same as entered password??
+
+            if(!isvalidPassword){
+                throw new Error("Invalid crediential")
+            }
+            else{
+                //creating a jwt token
+                const token=user.getJWT()  //we have simply used logic in schema methods instead here to make code more readable
+                
+                //sending cookie(jwt token)
+                //we can also expire cookie, httpOnly mean cookie will be stored for http site only not https
+                res.cookie("token",token,{expires: new Date(Date.now() + 8 * 3600000),httpOnly:true}) // cookie will be removed after 8 hours
+                res.send("Login successfully")
+                
+            }
+        }
+    } catch (error) {
+        res.send("ERORR:"+error.message)
+    }
+})
