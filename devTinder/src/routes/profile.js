f39ebcdcf7c67724dd0a9ecd28d637bfd6ca4f99
Adeel Haider <146270709+Adeel-Haider-03@users.noreply.github.com>
2025-08-19 23:41:30 +0500

    Update profile.js

--- devTinder/src/routes/profile.js
+++ devTinder/src/routes/profile.js
@@ -1,9 +1,9 @@
 const express=require('express')
 const router=express.Router();
-const {userAuth}=require('./middlewares/userAuth');
+const {userAuth}=require('../middlewares/userAuth');
 
 
-app.get('/profile',userAuth,async(req,res)=>{
+router.get('/profile',userAuth,async(req,res)=>{
     try {
         //we get user from req attached in middleware
         const user=req.user
@@ -13,3 +13,16 @@ app.get('/profile',userAuth,async(req,res)=>{
         res.send("ERROR:"+ error.message)
     }
 })
+
+router.patch('/update',async(req,res)=>{
+    try {
+        //await User.findByIdAndUpdate("68321c6d9f49640a83f33e96",{lastName:"Don"})
+
+        await User.findOneAndUpdate({lastName:"Boss"},{lastName:"Don"})
+        res.send("updated successfully")
+    } catch (error) {
+        res.send("update not successful")
+    }
+})
+
+module.exports=router;
